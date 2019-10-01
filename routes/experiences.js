446130db13d58edb9a444d5417661e3812586b81
Lena Lau <frau.lena.lau@gmail.com>
2019-10-01 21:32:24 +0200

    adds comment-function

--- routes/experiences.js
+++ routes/experiences.js
@@ -4,6 +4,7 @@ var router = express.Router();
 const User = require('../models/user')
 const Experience = require('../models/experience')
 const Location = require('../models/location')
+const Comment = require('../models/comment')
 
 //middleware
 const isAuthenticated = (req, res, next) => {
@@ -56,6 +57,34 @@ router.post('/:experience_id', (req, res, next) => {
       res.redirect('/profile')
     })
 });
+
+//GET /:id/edit 
+
+router.get('/:experience_id/add-comment', (req, res, next) => {
+  Experience.findById(req.params.experience_id).then((result) => {
+    console.log("result", result)
+    res.render('experiences/add-comment', result);
+  })
+});
+
+
+
+
+// POST /experiences/add-comment
+router.post('/:experience_id/comments', function (req, res, next) {
+  let { comment, owner } = req.body;
+  Comment.create({ comment, owner, owner: req.user, experience: req.params.experience_id })
+    .then(() => {
+      res.redirect('/profile');
+      console.log("Comment", comment, owner)
+    })
+
+})
+// Experience.update(
+//   { _id: req.params.experience_id },
+//   { title, plan, comments, locations, expireDate, owner: req.user }).then(() => {
+//     res.redirect('/profile')
+//   })
 
 router.post('/:experience_id/delete', (req, res, next) => {
   Experience.findByIdAndRemove({ _id: req.params.experience_id })
