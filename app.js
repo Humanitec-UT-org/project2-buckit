7dbe11db8dee631851c1b931031acd82decfd677
Chanty He <chanty.he90@googlemail.com>
2019-10-08 20:23:25 +0200

    Add environment variable for mongo db uri

--- app.js
+++ app.js
@@ -11,8 +11,7 @@ const flash = require("connect-flash")
 
 
 const mongoose = require('mongoose');
-const dbName = 'project2-buckit';
-mongoose.connect(`mongodb://localhost/${dbName}`);
+mongoose.connect(process.env.MONGODB_URI);
 
 
 const session = require("express-session");
